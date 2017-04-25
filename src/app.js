54ce1b9dca4c16b29d457bab1cbd9613b6611ca3
Mark C Mitchell <mark@markcmitchell.net>
2017-04-25 16:07:51 +1000

    add flickable circle

--- src/app.js
+++ src/app.js
@@ -3,23 +3,34 @@ import {
   AppRegistry,
   Text,
   StyleSheet,
-  View
+  View,
+  Dimensions
 } from 'react-native'
+import Interactable from 'react-native-interactable'
+
 
 export default class DontLeaveMeHangin extends React.Component {
   render() {
+    const {width, height} = Dimensions.get('window')
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Interactable.View
+          snapPoints={[]}
+          frictionAreas={[{damping: 0.82}]}
+          boundaries={{
+            top: -height/2,
+            left: -width/2,
+            bottom: height/2,
+            right: width/2,
+            bounce: 0.5
+          }}
+          initialPosition={{x: 0, y: 0}}
+        >
+          <View
+            style={{width: 70, height: 70, backgroundColor: 'aqua', borderRadius: 35}}
+          />
+        </Interactable.View>
       </View>
     );
   }
@@ -31,16 +42,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
