fb67d5ee9cc82eb47fdd605b882adfb39fefd108
Mark C Mitchell <mark@markcmitchell.net>
2017-04-25 20:36:22 +1000

    add text inside circle, background

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ import {
   Text,
   StyleSheet,
   View,
-  Dimensions
+  Dimensions,
+  TextInput,
+  Image
 } from 'react-native'
 import Interactable from 'react-native-interactable'
 
@@ -15,22 +17,39 @@ export default class DontLeaveMeHangin extends React.Component {
 
     return (
       <View style={styles.container}>
-        <Interactable.View
-          snapPoints={[]}
-          frictionAreas={[{damping: 0.82}]}
-          boundaries={{
-            top: -height/2,
-            left: -width/2,
-            bottom: height/2,
-            right: width/2,
-            bounce: 0.5
-          }}
-          initialPosition={{x: 0, y: 0}}
-        >
-          <View
-            style={{width: 70, height: 70, backgroundColor: 'aqua', borderRadius: 35}}
-          />
-        </Interactable.View>
+        <Image source={require('./bg.png')} style={{
+          resizeMode: 'cover',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',          
+        }}>
+          <Interactable.View
+            snapPoints={[]}
+            frictionAreas={[{damping: 0.85}]}
+            boundaries={{
+              top: -height/2,
+              left: -width/2,
+              bottom: height/2,
+              right: width/2,
+              bounce: 0.5
+            }}
+            initialPosition={{x: 0, y: 0}}
+            animatedNativeDriver={true}
+          >
+            <View style={{
+              width: 85, 
+              height: 85, 
+              backgroundColor: '#0c64e8', 
+              borderRadius: 43,
+              flex: 0,
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}>
+              <Text style={{fontSize: 20, color: 'white'}}>Luke</Text>
+            </View>
+          </Interactable.View>
+
+        </Image>
       </View>
     );
   }
@@ -41,7 +60,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
 });
 
